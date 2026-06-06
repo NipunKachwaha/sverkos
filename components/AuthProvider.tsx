4941149385f406ca12be9c2bb29d326c381a44fc
Nipun <nipunrajput077@gmail.com>
2026-06-06 20:01:21 +0530

    hie

--- components/AuthProvider.tsx
+++
@@ -1,8 +0,0 @@
-// components/AuthProvider.tsx
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
-export default function AuthProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
